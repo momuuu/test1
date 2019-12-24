471ac96758ce9f167ab3b6bac9a798a74996b15e
cqb194 <cqb194@qq.com>
2019-12-24 14:44:11 +0800

    get header line,clear header,get method from header

--- main.c
+++ main.c
@@ -11,9 +11,57 @@
 #include <pthread.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#define SERVER "Server: simple web server by momu\r\n"
+
 
 void *accept_request(void*);
 int init_listen_sock(u_short *port);
+int getLine(int, char* , int);
+void send_httpok_header(int);
+void clear_header(int);
+
+void clear_header(int client){
+    char buf[128];
+    memset(buf, 0, sizeof(buf));
+    int n = 1;
+    while((n > 0) && strcmp("\n", buf)){
+        n = getLine(client, buf, sizeof(buf));
+    }
+}
+
+void send_httpok_header(int client){
+    char buf[1024];
+    strcpy(buf, "HTTP/1.0 200 OK\r\n");
+    send(client, buf, strlen(buf), 0);
+    strcpy(buf, SERVER);
+    send(client, buf, strlen(buf), 0);
+    strcpy(buf, "Content-Type: text/html;charset=UTF-8\r\n\r\n");
+    send(client, buf, strlen(buf), 0);
+}
+
+int getLine(int client, char *buf, int buf_size){
+    int i = 0;
+    char c = '\0';
+    int n = 0;
+    while((i < buf_size - 1) && (c != '\n')){
+        n = recv(client, &c, 1, 0);
+        if (n > 0){
+            if(c == '\r'){
+                n = recv(client, &c, 1, MSG_PEEK);
+                if((n > 0) && (c == '\n'))
+                    recv(client, &c, 1, 0);
+                else
+                    c = '\n';
+            }
+            buf[i] = c;
+            i ++;
+        }else {
+            c = '\n';
+        }
+    }
+    buf[i] = '\0';
+    return i;
+}
 
 void error_die(const char* msg){
     perror(msg);
@@ -23,16 +71,23 @@ void error_die(const char* msg){
 void *accept_request(void* client_sock){
     int client = *(int *)client_sock;
     char buf[1024];
-    char buffer[1024];
-    recv(client, buf, 1024, 0);
-    sprintf(buffer, "HTTP/1.0 200 OK\r\n");
-    send(client, buffer, strlen(buffer), 0);
-    sprintf(buffer, "Content-Type: text/html\r\n");
-    send(client, buffer, strlen(buffer), 0);
-    sprintf(buffer, "\r\n");
-    send(client, buffer, strlen(buffer), 0);
-    sprintf(buffer, "testing");
-    send(client, buffer, strlen(buffer), 0);
+    char method[256];
+    size_t i = 0;
+    size_t j = 0;
+    getLine(client, buf, 1024);
+    while (!isspace((int)buf[j]) && (i < sizeof(method) - 1)){
+        method[i] = buf[j];
+        i++;
+        j++;
+    }
+    clear_header(client);
+    method[i] = '\0';
+    send_httpok_header(client);
+    if (strcasecmp(method, "GET") && strcasecmp(method, "POST")){
+        sprintf(method, "unable method");
+    }
+    send(client, method, strlen(method), 0);
+   
     close(client);
     return NULL;
 }
